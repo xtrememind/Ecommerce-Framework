5c1ca03dfea5b7f0bd8045ca59824768079cf836
Mohamed Elsayed <mohammed.elsayed@gmail.com>
2018-05-16 15:02:56 -0500

    testing with the database

---
+++ src/main/java/com/ecf/EcommerceFrameworkApplication.java
@@ -0,0 +1,12 @@
+package com.ecf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFrameworkApplication.class, args);
+	}
+}
