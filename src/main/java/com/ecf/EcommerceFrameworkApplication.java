c824fcd22f2f101601c7c4a732b158d3ed38616a
shamalMUM <shamalf1@gmail.com>
2018-05-17 16:14:48 -0500

    added UI

--- src/main/java/com/ecf/EcommerceFrameworkApplication.java
+++ src/main/java/com/ecf/EcommerceFrameworkApplication.java
@@ -2,8 +2,10 @@ package com.ecf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com")
 public class EcommerceFrameworkApplication {
 
 	public static void main(String[] args) {
