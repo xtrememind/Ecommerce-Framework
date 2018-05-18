70692e05899532940a8bfbd1175d6e319317661d
mena <mena.emile@gmail.com>
2018-05-17 22:07:15 -0500

    modification on logging and payment

--- src/main/java/com/ecf/Patterns/Logging/Consolelog.java
+++ src/main/java/com/ecf/Patterns/Logging/Consolelog.java
@@ -2,7 +2,7 @@ package com.ecf.Patterns.Logging;
 
 public class Consolelog extends Logging {
     @Override
-    public void log(Loginfo logInfo) {
+    public void log(LogInfo logInfo) {
 
     }
 }
--- src/main/java/com/ecf/Patterns/Logging/Databaselog.java
+++ src/main/java/com/ecf/Patterns/Logging/Databaselog.java
@@ -2,7 +2,7 @@ package com.ecf.Patterns.Logging;
 
 public class Databaselog extends Logging {
     @Override
-    public void log(Loginfo logInfo) {
+    public void log(LogInfo logInfo) {
 
     }
 }
