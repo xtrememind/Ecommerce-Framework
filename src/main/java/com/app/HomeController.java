733cea44a32d6594e7671ea547061d380b734379
Shamal Fernando <shamalf1@gmail.com>
2018-05-19 22:16:01 -0500

    Store repo connection

--- src/main/java/com/app/HomeController.java
+++ src/main/java/com/app/HomeController.java
@@ -1,16 +1,32 @@
 package com.app;
 
+import com.ecf.Repository.StoreRepository;
+import com.ecf.domain.Category;
+import com.ecf.domain.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-
+    public String greeting(@RequestParam(name="id", required=false) Integer id, Model model)
+    {
+        List<Product> products;
+        StoreRepository storeRepository =StoreRepository.getInstance();
+        if(id==null) {
+            products = storeRepository.getAllProduct();
+        }
+        else{
+            products = storeRepository.getCategoryProduct(id);
+        }
+        List<Category> categoryList=storeRepository.getCategoriesList();
+        model.addAttribute("categoryList",categoryList);
+        model.addAttribute("products",products);
         return "home";
     }
 }
--- src/main/java/com/ecf/Repository/StoreRepository.java
+++ src/main/java/com/ecf/Repository/StoreRepository.java
@@ -4,17 +4,20 @@ import com.ecf.domain.Category;
 import com.ecf.domain.Product;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StoreRepository {
 
-    private static StoreRepository userRepository;
-    List<Category> categoriesList;
+    private static StoreRepository storeRepository;
+    private List<Category> categoriesList;
 
     public static StoreRepository getInstance() {
-        if (userRepository == null)
-            userRepository = new StoreRepository();
-        return userRepository;
+        if (storeRepository == null)
+            storeRepository = new StoreRepository();
+        return storeRepository;
     }
 
     private StoreRepository() {
@@ -22,23 +25,23 @@ public class StoreRepository {
         Category electronics = new Category("Electronics");
         electronics.addSubCategory(new Category("Mobile Phones"));
         electronics.addSubCategory(new Category("Televisions"));
-        electronics.addProducts(new Product("Apple Macbook Pro","Best laptop ever", 1000.00));
-        electronics.addProducts(new Product("Apple iPhone","Best mobile ever", 900.00));
-        electronics.addProducts(new Product("Apple Watch","Best watch ever", 400.00));
+        electronics.addProducts(new Product("Apple Macbook Pro", "Best laptop ever", 1000.00));
+        electronics.addProducts(new Product("Apple iPhone", "Best mobile ever", 900.00));
+        electronics.addProducts(new Product("Apple Watch", "Best watch ever", 400.00));
         categoriesList.add(electronics);
 
         Category officeSupplies = new Category("Office Supplies");
         officeSupplies.addSubCategory(new Category("Chairs"));
         officeSupplies.addSubCategory(new Category("Desks"));
-        officeSupplies.addProducts(new Product("Zen Desk","Best desk ever", 400.00));
-        officeSupplies.addProducts(new Product("Zen Chair","Best chair ever", 400.00));
+        officeSupplies.addProducts(new Product("Zen Desk", "Best desk ever", 400.00));
+        officeSupplies.addProducts(new Product("Zen Chair", "Best chair ever", 400.00));
         categoriesList.add(officeSupplies);
 
         Category appliances = new Category("Appliances");
         appliances.addSubCategory(new Category("ovens"));
         appliances.addSubCategory(new Category("Microwaves"));
-        appliances.addProducts(new Product("Toshiba Microwave","Best microwave ever", 200.00));
-        appliances.addProducts(new Product("Samsung Microwave","Best microwave ever", 300.00));
+        appliances.addProducts(new Product("Toshiba Microwave", "Best microwave ever", 200.00));
+        appliances.addProducts(new Product("Samsung Microwave", "Best microwave ever", 300.00));
         categoriesList.add(appliances);
     }
 
@@ -47,10 +50,10 @@ public class StoreRepository {
         categoriesList.add(category);
     }
 
-    public Category findCategory(Category category) {
+    public Category findCategory(Integer id) {
         try {
             Category cat = categoriesList.stream()
-                    .filter(u -> u.getName().equals(category.getName()))
+                    .filter(u -> u.getId()==id)
                     .findFirst()
                     .get();
             return cat;
@@ -62,4 +65,17 @@ public class StoreRepository {
     public List<Category> getCategoriesList() {
         return categoriesList;
     }
+
+    public List<Product> getAllProduct() {
+        return categoriesList.stream().map(c -> c.getProducts()).
+                        flatMap(c -> c.stream()).collect(Collectors.toList());
+    }
+
+    public List<Product> getCategoryProduct(Integer categoryId) {
+        return categoriesList.stream().filter(c->c.getId().equals(categoryId))
+                .map(c -> c.getProducts())
+                .flatMap(c -> c.stream()).collect(Collectors.toList());
+    }
+
+
 }
