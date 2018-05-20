241b89f5dd13f42bc57e1575389fdff80fcc5007
Shamal Fernando <shamalf1@gmail.com>
2018-05-20 15:06:36 -0500

    registration changes

--- src/main/java/com/ecf/registration/RegisterFactory.java
+++ src/main/java/com/ecf/registration/RegisterFactory.java
@@ -1,11 +1,12 @@
 package com.ecf.registration;
 
 
+import com.ecf.domain.Address;
 import com.ecf.registration.models.User;
 
 public interface RegisterFactory {
 
-    public User createUser(String type);
+    public void createUser(String name, String pass, Address s,Address b, String type);
 
 
 }
--- src/main/java/com/ecf/registration/UserRegisterFactory.java
+++ src/main/java/com/ecf/registration/UserRegisterFactory.java
@@ -1,33 +1,38 @@
 package com.ecf.registration;
 
+import com.ecf.Repository.UserRepository;
 import com.ecf.domain.Address;
 import com.ecf.domain.Client;
+import com.ecf.registration.models.Admin;
 import com.ecf.registration.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserRegisterFactory implements RegisterFactory {
-
-
-
-    public UserRegisterFactory(){
+    User user;
+    Address address;
+    UserRepository userRepo;
+    public UserRegisterFactory() {
+        userRepo=UserRepository.getInstance();
     }
 
     @Override
-    public User createUser(String type) {
-//        Client client =new Client();
-//		client.setName("Test");
-//
-		Address add =new Address();
-		add.setCity("City");
-		add.setState("state");
-		add.setStreetName("street");
-//
-//		client.setBillingAddress(add);
-//		client.setShippingAddress(add);
-//
+    public void createUser(String name, String pass, Address s,Address b, String type){
 
-        return null;
+        switch (type) {
+            case "CLIENT": {
+                user=new Client(name,pass,false,s,b);
+                userRepo.addUser(user);
+                break;
+            }
+            case "ADMIN": {
+                user=  user=new Admin(name,pass,true);
+                userRepo.addUser(user);
+                break;
+            }
+        }
+
     }
+
 }
